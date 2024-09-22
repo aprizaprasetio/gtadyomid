471c5ee7eb536045652a76838300d9d06ba6aa03
Apriza <aprizaprasetio@gmail.com>
2024-09-22 13:43:52 +0700

    ✨ added login user

--- apps/server/src/apis/users.api.ts
+++ apps/server/src/apis/users.api.ts
@@ -2,12 +2,17 @@ import { password } from 'bun'
 import { Hono } from 'hono'
 import { validator } from 'hono/validator'
 import { z, ZodError, type ZodIssue } from 'zod'
+import { auth } from '@src/clients/auth.client'
 import { db } from '@src/clients/db.client'
 
-const registerSchema = z.object({
+export const registerSchema = z.object({
   email: z.string().email().toLowerCase(),
   username: z.string().toLowerCase(),
   displayName: z.string().nullish(),
+  password: z.string(),
+})
+export const loginSchema = z.object({
+  identity: z.string().toLowerCase(),
   password: z.string(),
 })
 
@@ -64,6 +69,59 @@ users.post(
         hashedPassword: await password.hash(rawPassword),
       },
     })
+
+    return c.body(null, 204)
+  },
+)
+
+users.post(
+  '/login',
+  validator('json', async (val, c) => {
+    const { error, data } = await loginSchema.safeParseAsync(val)
+    if (error) return c.json(error, 400)
+
+    const user = await db.user.findFirst({
+      where: {
+        OR: [
+          {
+            email: data.identity,
+          },
+          { username: data.identity },
+        ],
+      },
+      select: {
+        id: true,
+        hashedPassword: true,
+      },
+    })
+
+    const loginError = new ZodError([
+      {
+        code: 'custom',
+        path: ['identity'],
+        message: 'Email or username might be wrong',
+      },
+      {
+        code: 'custom',
+        path: ['password'],
+        message: 'Password might be wrong',
+      },
+    ])
+
+    if (!user) return c.json(loginError, 400)
+
+    const isVerified = await password.verify(data.password, user.hashedPassword)
+
+    if (isVerified) return user.id
+
+    return c.json(loginError, 400)
+  }),
+  async c => {
+    const userId = c.req.valid('json')
+    const session = await auth.createSession(userId, {})
+    const sessionCookie = auth.createSessionCookie(session.id)
+
+    c.header('Set-Cookie', sessionCookie.serialize())
 
     return c.body(null, 204)
   },
