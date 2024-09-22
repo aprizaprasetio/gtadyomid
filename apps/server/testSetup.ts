475e64eed1fbcdcfafcea4a07fac41081fe27c23
Apriza <aprizaprasetio@gmail.com>
2024-09-22 13:45:13 +0700

    ✅ updated test setup hooks

--- apps/server/testSetup.ts
+++ apps/server/testSetup.ts
@@ -1,11 +1,30 @@
-import { afterAll, beforeEach } from 'bun:test'
+import { afterAll, beforeAll } from 'bun:test'
+import type { z } from 'zod'
 import { db } from './src/clients/db.client'
+import { users, type registerSchema } from './src/apis/users.api'
 
-beforeEach(async () => {
-  // Clean the test database before each test
-  await db.user.deleteMany({})
+export const testUser: z.infer<typeof registerSchema> = {
+  email: 'test@example.com',
+  username: 'testuser',
+  password: 'testpassword123',
+  displayName: 'Test User',
+}
+
+export function registerUser(user: Partial<z.infer<typeof registerSchema>>) {
+  return users.request('/register', {
+    method: 'POST',
+    body: JSON.stringify(user),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+}
+
+beforeAll(async () => {
+  await registerUser(testUser)
 })
 
 afterAll(async () => {
+  await db.user.deleteMany()
   await db.$disconnect()
 })
