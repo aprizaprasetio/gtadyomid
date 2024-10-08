2f390d1e3591b45c836af48e14849afd83e3dfc3
Apriza <aprizaprasetio@gmail.com>
2024-10-09 06:29:04 +0700

    🏷️ added session type from hono context variable map

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,5 +1,12 @@
 import { Hono } from 'hono'
+import type { Session } from 'lucia'
 import { users } from './apis/users.api'
+
+declare module 'hono' {
+  interface ContextVariableMap {
+    session: Session | null
+  }
+}
 
 const app = new Hono()
 
