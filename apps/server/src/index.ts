768a0fc55e183631ffc4ebcbe500421486a2350e
Apriza <aprizaprasetio@gmail.com>
2025-01-12 10:36:18 +0700

    🏷️ added env type

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -4,6 +4,16 @@ import { users } from './apis/users.api'
 import { missions } from './apis/missions.api'
 import { sessionMiddleware } from './utils/session.middleware'
 
+declare module 'bun' {
+  interface Env {
+    DATABASE_URL: string
+    S3_ACCESS: string
+    S3_SECRET: string
+    S3_BUCKET: string
+    S3_URL: string
+  }
+}
+
 declare module 'hono' {
   interface ContextVariableMap {
     session: Session | null
